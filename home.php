1f24aa376060388fc95dad498004c16ad14b8158
program-cloud <mahajanlakhan79@gmail.com>
2021-06-25 13:14:31 +0530

    new file added

---
+++ home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+<?php include 'links.php' ?>
+<link rel="stylesheet" href="">
+
+</head>
+<body> 
+    <nav class="navbar ">
+        <div class="container">
+            <article>
+                <a  href="#" class="navbar-brand">welcome
+
+                </a>
+                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#homepage">
+                    <span class="navbar-toggle-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collpase" id="homepage">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Home
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Gallary
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">About
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Logout
+                        </li>
+                    </ul>
+                </div>
+
+
+                
+            </article>
+        </div>
+    </nav>
+</body>
+</html>
