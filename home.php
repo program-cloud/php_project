4eb71aa1f689329548a13e563be193eb7685d44b
program-cloud <mahajanlakhan79@gmail.com>
2021-06-25 14:07:46 +0530

    home page

---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'links.php' ?>
+
+  </head>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+
+    <div class="container">
+
+            
+  <a class="navbar-brand" href="#">welcome</a>
+
+
+       
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbaraid">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class=" collapse navbar-collapse" id="navbaraid">
+        
+               <ul class="navbar-nav text-center ml-auto">
+                    <li class="nav-item">
+                         <a href="#" class="nav-link">Home</a>
+                     </li>
+                     <li class="nav-item">
+                        <a href="#" class="nav-link">About</a>
+                     </li>
+                     <li class="nav-item">
+                        <a href="#" class="nav-link">Gallary</a>
+                        </li>
+                      <li class="nav-item">
+                          <a href="#" class="nav-link">Logout</a>
+                       </li>
+                     
+               </ul>
+           </div>
+   </div>
+
+</nav>
